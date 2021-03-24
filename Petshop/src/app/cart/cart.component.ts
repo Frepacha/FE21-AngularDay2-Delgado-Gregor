333309fc7574f27e3079d3cb58e8eefccde9d37e
Freddy <freddy.delgado.porras@gmail.com>
2021-03-24 14:47:47 +0100

    we added cart items to cart.service.ts

--- Petshop/src/app/cart/cart.component.ts
+++ Petshop/src/app/cart/cart.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+// tslint:disable-next-line: max-line-length
+/* We added the items in the ngOnInit() function, because it will be loaded every time we enter the component or we go to the cart component. */
 export class CartComponent implements OnInit {
-
-  constructor() { }
+petitems;
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.petitems =  this.cartService.getItems();
   }
 
 }
