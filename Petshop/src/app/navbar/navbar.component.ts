5adf6f681f25b48a39f73b60e9b4aa6d40f2e465
Freddy <freddy.delgado.porras@gmail.com>
2021-03-24 19:17:27 +0100

    i added the chackout itemLength

--- Petshop/src/app/navbar/navbar.component.ts
+++ Petshop/src/app/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
+export class NavbarComponent implements OnInit, DoCheck {
+cartLength;
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line: typedef
+  ngDoCheck(){
+    this.cartLength = this.cartService.itemsLength();
+  }
 }
